03b76ddf7d7f665b41b6d31d0822b1ac4cf270f9
Mauricio <mauricio.o.mesquita@gmail.com>
2026-09-06 00:24:23 -0400

    Fix env issues
    Fix env issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopackUseSystemTlsCerts: true,
+  },
+  env: {
+    NEXT_PUBLIC_CONVEX_URL:
+      process.env.NEXT_PUBLIC_CONVEX_URL ?? process.env.CONVEX_URL,
+  },
 };
 
 export default nextConfig;
